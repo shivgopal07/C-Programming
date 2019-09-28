ed7590a59e7d9c9cbe6d14f2471be3a656b94947
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2019-09-28 18:42:27 +0530

    area, perimeter and vol of circle

---
+++ circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float r,A,P,V;
+    printf("\n enter radious");
+    scanf("%f",&r);
+    A=r*r;
+    P=2*3.14*r;
+    V=r*r*r;
+    printf("\narea is: %f,  perimeter is: %f,  volume is: %f",A,P,V);
+
+}
