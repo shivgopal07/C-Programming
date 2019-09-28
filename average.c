fd02a388b5b952027f9a3f75c2f4ad10a3fbfa93
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2019-09-28 17:59:04 +0530

    average of three numbers

---
+++ average.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float a,b,c,avg;
+    printf("\nenter no's");
+    scanf("\n%f%f%f",&a,&b,&c);
+    avg=(a+b+c)/3;
+    printf("average is:%f",avg);
+    getch();
+}
