12cf3bece877cc65cfce537153cd004483b17490
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2019-09-29 12:13:16 +0530

    larger one btw two no

---
+++ large_no.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("\nEnter two no: ");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    printf("\n %d is large no",a);
+    else
+    printf("\n %d is large no",b);
+}
