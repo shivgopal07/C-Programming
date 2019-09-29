12e73805afc5cc142e8fe48f870949ca676db0ed
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2019-09-29 12:11:48 +0530

    no is positive or negative

---
+++ pos_neg.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("\nEnter a number: ");
+    scanf("%d",&n);
+    if (n>=0)
+        printf("\nThe number is positive ");
+    else
+        printf("\nThe number is negative ");
+}
