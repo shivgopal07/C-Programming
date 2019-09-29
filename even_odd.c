e32f97c5e322061a78489dc5cd8dbe62f30c6acd
Chaitaly Kundu <chaitaly.slg01@gmail.com>
2019-09-29 12:14:11 +0530

    the no is even or odd

---
+++ even_odd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("\nEnter a no: ");
+    scanf("%d",&n);
+    if(n%2==0)b
+        printf("\nThe no is even");
+    else
+        printf("\nThe no is odd");
+}
