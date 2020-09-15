ffc4c9adbce40f3ab98fae07b0ebd5e23edeebc3
Shiv Gopal Verma <56026986+shivgopal07@users.noreply.github.com>
2020-09-15 23:30:59 +0530

    this shows in which category the citizens are.
    By this code, we can know that person is child, adult or senior citizen.

--- citizens age.c
+++ citizens age.c
@@ -3,9 +3,11 @@ int main()
 {
     int n;
     printf("\nEnter age: ");
-    scanf("\n%d",&n);
+    scanf("\n%d",&n)
     if(n>=60)
         printf("\nThe person is senior citizen");
+    else if(n>=18)
+        printf("\nThe person is adult citizen");
     else
-        printf("\nThe person is not senior citizen");
+        printf("\nThey are children");
 }
